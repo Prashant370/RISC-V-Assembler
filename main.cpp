3f72f87f0c7fc81452cd574459d4e1f0cf5f68f5
kartikey sahu <kartikeysahu987@gmail.com>
2024-03-01 12:02:36 +0530

    first main

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#include <fstream>
+
+void getopCode(string have, string ){ // For R,I,S,SB,U,UJ Format
+    
+}
+void getFunc3(){  // For R,I,S,SB Format
+
+}
+void getFunc7(){ // For R Format
+
+}
+void getImmediate(){ // For I,S,SB,U,UJ Format
+
+}
+void getRegisterValue(){ // For R,I,S,SB,,U,UJ Format
+    
+}
+
+
+int main()
+{
+    
+    ofstream fout;
+    ifstream fin;
+
+    string line;
+    while (getline (fin, line)) {
+        
+    }
+    fin.close();
+    fout.close();
+    return 0;
+}
